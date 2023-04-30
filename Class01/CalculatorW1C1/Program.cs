b31f88369d0b59d084045286eea03c7d06cbec47
Ibrahim Swaiss <Iaswaiss@outlook.com>
2023-05-01 00:53:24 +0300

    add calculator app

---
+++ Class01/CalculatorW1C1/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Enter First Number");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter Second Number");
+int y = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine("The result is: ");
+//int result = x + y;
+//Console.WriteLine(result);
+
+Console.WriteLine(x + y);
+
+//Console.ReadLine();
