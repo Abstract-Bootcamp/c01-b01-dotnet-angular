fe416d11a59691e1263960b074a49f3dacdc443a
Ibrahim Swaiss <Iaswaiss@outlook.com>
2023-05-02 20:58:45 +0300

    add class 2

--- Class01/CalculatorW1C1/Program.cs
+++ Class01/CalculatorW1C1/Program.cs
@@ -1,14 +1,23 @@
 ﻿
-Console.WriteLine("Enter First Number");
+Console.WriteLine("Enter First Number test");
+Console.ForegroundColor = ConsoleColor.Red;
 int x = int.Parse(Console.ReadLine());
+Console.ForegroundColor = ConsoleColor.Blue;
 Console.WriteLine("Enter Second Number");
+
+
+Console.ResetColor();
+
 int y = int.Parse(Console.ReadLine());
 
+Console.BackgroundColor = ConsoleColor.Green;
 
 Console.WriteLine("The result is: ");
 //int result = x + y;
 //Console.WriteLine(result);
 
-Console.WriteLine(x + y);
+int result = x + y;
+
+Console.WriteLine(result);
 
 //Console.ReadLine();
