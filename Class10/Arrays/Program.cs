596ac9072c50883d8c94ebe8f3670109efa88e89
Ibrahim Swaiss <Iaswaiss@outlook.com>
2023-05-22 17:50:35 +0300

    w4c10

---
+++ Class10/Arrays/Program.cs
@@ -0,0 +1,28 @@
+﻿int[] x = new int[10];
+for (int i = 0; i < x.Length; i++)
+{
+    x[i] = x[i] * 5;
+}
+// [0, 5, 10, 20, 30, 40, 50, 60, 70, 80]
+int[] y = new int[10];
+for (int i = 0; i < y.Length; i++)
+{
+    y[i] = y[i] * 5;
+}
+// [0, 5, 10, 20, 30, 40, 50, 60, 70, 80]
+
+
+// find a value of 70 in x and then get the index of the same value in y
+for (int i = 0; i < x.Length; i++)
+{
+    if (x[i] == 70)
+    {
+        for (int j = 0; j < y.Length; i++)
+        {
+            if (y[j] == x[i])
+            {
+                Console.WriteLine("The index of 70 in y is: " + j);
+            }
+        }
+    }
+}
---
+++ Class10/ng-intor/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'ng-intor';
+  textPlaceholder = 'Enter your username';
+  passwordPlaceholder = 'Enter your password';
+
+  username = 'Mohammad';
+  password = '';
+
+  welcomeMessage = '';
+
+  login() {
+    this.welcomeMessage = 'Welcome ' + this.username;
+  }
+}
