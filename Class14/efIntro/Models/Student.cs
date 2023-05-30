27a8efb915a69945c1612caaa853b6cda04d93ed
Ibrahim Swaiss <Iaswaiss@outlook.com>
2023-05-30 04:07:17 +0300

    entity framework

---
+++ Class14/efIntro/Models/Student.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace efIntro.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; } = "";
+    }
+}
