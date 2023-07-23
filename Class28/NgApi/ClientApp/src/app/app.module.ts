576d107a138a2e48b8f4f88c510c1e3ef882eca4
Ibrahim Swaiss <Iaswaiss@outlook.com>
2023-07-23 17:45:03 +0300

    fetch data

--- Class28/NgApi/ClientApp/src/app/app.module.ts
+++ Class28/NgApi/ClientApp/src/app/app.module.ts
@@ -1,15 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +28,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    MatToolbarModule, MatButtonModule, MatIconModule, MatTableModule, MatMenuModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
--- Class28/NgApi/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ Class28/NgApi/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -1,17 +1,22 @@
-import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Component, Inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-fetch-data',
   templateUrl: './fetch-data.component.html'
 })
-export class FetchDataComponent {
+export class FetchDataComponent implements OnInit {
   public forecasts: WeatherForecast[] = [];
+  displayedColumns: string[] = ['date', 'temperatureC', 'temperatureF', 'summary', 'actions'];
 
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<WeatherForecast[]>(baseUrl + 'weatherforecast').subscribe(result => {
+  constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {
+  }
+
+  ngOnInit(): void {
+    const forecasts$ = this.http.get<WeatherForecast[]>(this.baseUrl + 'weatherforecast');
+    forecasts$.subscribe(result => {
       this.forecasts = result;
-    }, error => console.error(error));
+    });
   }
 }
 
