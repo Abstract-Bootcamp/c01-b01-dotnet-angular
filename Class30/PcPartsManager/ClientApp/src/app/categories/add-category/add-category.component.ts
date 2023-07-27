c7bad4fce3a41844a409a2cec8535615f573e60d
Ibrahim Swaiss <Iaswaiss@outlook.com>
2023-07-27 20:22:53 +0300

    add pagination

--- Class30/PcPartsManager/ClientApp/src/app/categories/add-category/add-category.component.ts
+++ Class30/PcPartsManager/ClientApp/src/app/categories/add-category/add-category.component.ts
@@ -1,21 +1,68 @@
+import { catchError, EMPTY, map } from 'rxjs';
+
 import { HttpClient } from '@angular/common/http';
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Category } from '../interfaces/category';
 
 @Component({
   selector: 'app-add-category',
   templateUrl: './add-category.component.html',
   styleUrls: ['./add-category.component.scss']
 })
-export class AddCategoryComponent {
+export class AddCategoryComponent implements OnInit {
+  id: number = 0;
   name: string = '';
 
-  constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) { }
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    route: ActivatedRoute,
+    @Inject('BASE_URL') private baseUrl: string
+  ) {
+    this.id = +route.snapshot.params['id'];
+  }
 
-  addCategory() {
+  ngOnInit(): void {
+    if (this.id > 0) {
+      this.http.get<Category>(this.baseUrl + 'category/get/' + this.id)
+        .pipe(
+          map(result => {
+            return result.name;
+            // result.name = result.name.toUpperCase();
+            // return result;
+          }),
+          catchError((error) => {
+            console.error(error);
+            return EMPTY;
+          }))
+        .subscribe(result => {
+          this.name = result;
+        });
+    }
+  }
+
+  addOrEditCategory() {
     const body = {
       name: this.name
     };
 
-    this.http.post(this.baseUrl + 'category/post', body).subscribe();
+    let observable = null;
+    if (this.id > 0) {
+      observable = this.http.put(this.baseUrl + 'category/edit/' + this.id, body);
+    } else {
+      observable = this.http.post(this.baseUrl + 'category/post', body);
+    }
+
+    observable
+      .pipe(
+        catchError((error) => {
+          console.error(error);
+          return EMPTY;
+        }))
+      .subscribe(() => {
+        this.router.navigate(['/categories']);
+      });
   }
 }
